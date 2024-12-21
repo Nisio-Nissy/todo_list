a1549593ecc5871c694ad8a986bb795133e8ff79
NissyLife <yagi3010@gmail.com>
2024-12-21 20:47:35 +0900

    修正内容: TodoFormのバグ修正と不要ファイル追加

--- TodoForm/index.js
+++ TodoForm/index.js
@@ -1,13 +1,10 @@
-//コメントはスラッシュを二つ書く
+
 
 const form = document.getElementById('form');
-//フォームの値を取得する（ここでidタグが役にたつ）
 
 const input = document.getElementById('input');
-//インプットの値を取得する
 
 const ul = document.getElementById('ul');
-//ulのタグの値を取得する
 
 const todos = JSON.parse(localStorage.getItem('todos'));
 
@@ -20,55 +17,55 @@ if(todos){
     })
 }
 
-//エンターキーを押す（submitする）時に行う内容
+
 form.addEventListener('submit', function (event){
     event.preventDefault();  
-    //デフォルトのイベント（今回は画面のリロード）を行わないようにする
+    
    add();
-   //addの関数は以下の通り
+   
 });
 
 function add(todo){
-    //inputタグ内でユーザーが書いた内容（value）を変数で定義する。
+    
     let todoText = input.value; 
 
     if(todo){
        todoText = todo.text;
     }
-    //条件分岐　書かれた内容が0文字以上なら実行される（暗黙の型変換）
+    
     if(todoText){
-     //定数li にDOM（ドキュメントのこと）で要素を作り出す。
+     
     const li = document.createElement('li');
-    //上記のcreatElementで定義されたliタグに含まれるテキスト範囲（innerText）を指定している。
+    
     li.innerText = todoText
-    //クラスリストの追加
+    
     li.classList.add('list-group-item');
-    //条件分岐　できたTODOリストを線で消す。
+    
     if(todo && todo.completed){
         li.classList.add('text-decoration-line-through');
     }
-    //contextmenu時に機能する内容
+    
     li.addEventListener('contextmenu', function(event){
         event.preventDefault();
         li.remove();
         saveData();
     });
-    //クリック時に機能する内容
+    
     li.addEventListener('click', function(){
         li.classList.toggle('text-decoration-line-through');
         saveData();
     })
     ul.appendChild(li);
-    //liタグを追加
+    
     input.value = "";
-    //中身を空にする
+    
     saveData();
-    //データを保存
+    
     }
 };
 
 function saveData(){
-    //liタグの全てのを選択する
+    
     const lists = document.querySelectorAll('li');
     let todos = [];
 
